d56d9a955267d76e05c5a4f04679d8f882dcbf83
Khaerius <33125280+Khaerius@users.noreply.github.com>
2020-11-18 12:52:43 +0100

    Created bounces
    Bounce system created with score and restarting game after point.  Ball moving only diagonally, which make sense (vide og Pong)

--- DisplayScreen.asm
+++ DisplayScreen.asm
@@ -12,11 +12,11 @@
 .REG s8, bally
 .REG s9, leftpad
 .REG sA, rightpad
-.REG sB, movex
-.REG sC, movey
-.REG s0, zero
+.REG sB, scorep1
+.REG sC, scorep2
 .REG sF, flagdir
 .CONST finish, 68
+.CONST zero, 0
 
 .DSEG
 ceiling: .DB "-----------------------------------------",13,10
@@ -27,18 +27,17 @@ floor: .DB "-----------------------------------------[H",10
 .CSEG
 LOAD s7, 0b00000100
 LOAD tmp, 0b00000001
-LOAD zero, 0
 OUT s7, int_mask
 EINT
 LOAD ballx, 20
 LOAD bally, 12
 LOAD leftpad, 12
 LOAD rightpad, 12
-LOAD counterx, 0
-LOAD countery, 0
-LOAD movex, 1
-LOAD movey, 0
-LOAD flagdir, 1
+LOAD counterx, zero
+LOAD countery, zero
+LOAD flagdir, zero
+LOAD scorep1, zero
+LOAD scorep2, zero
 LOAD string, ceiling
 OUT tmp, uart0_mask
 loop: JUMP loop
@@ -56,7 +55,8 @@ int: FETCH char, string
 			OUT char, uart0
 			ADD string, 1
 			ADD counterx, 1
-			end: OUT zero, int_status
+			LOAD tmp, zero
+			end: OUT tmp, int_status
 			RETI
 
 nxverse: ADD countery, 1
@@ -91,9 +91,9 @@ LOAD char, '|'
 RET
 
 checkpos: COMP bally, 1
-JUMP Z, checkcontl
+JUMP Z, checkcontu
 COMP bally, 23
-JUMP Z, checkcontl
+JUMP Z, checkcontd
 COMP ballx, 1
 JUMP Z, checkcontl
 COMP ballx, 39
@@ -101,23 +101,64 @@ JUMP Z, checkcontr
 RET
 
 checkcontl: COMP bally, leftpad
-JUMP NZ, skip
+JUMP NZ, addpoint
+COMP flagdir, 2
+JUMP NZ, elsel
 LOAD flagdir, 1
+RET
+elsel: LOAD flagdir, 0
 RET
 
 checkcontr: COMP bally, rightpad
-JUMP NZ, skip
-LOAD flagdir, 0
+JUMP NZ, addpoint
+COMP flagdir, 1
+JUMP NZ, elser
+LOAD flagdir, 2
+RET
+elser: LOAD flagdir, 3
 RET
 
-moveball: COMP flagdir, 1
+addpoint: COMP ballx, 1
+JUMP NZ, elsea
+ADD scorep2, 1
+LOAD ballx, 20
+LOAD bally, 12
+RET
+elsea: ADD scorep1, 1
+LOAD ballx, 20
+LOAD bally, 12
+RET
+
+checkcontu: COMP flagdir, 0
+JUMP NZ, elseu
+LOAD flagdir, 1
+RET
+elseu: LOAD flagdir, 2
+RET
+
+checkcontd: COMP flagdir, 1
+JUMP NZ, elsed
+LOAD flagdir, 0
+RET
+elsed: LOAD flagdir, 3
+RET
+
+moveball: COMP flagdir, 0
 JUMP NZ, else
-ADD ballx, movex
-ADD bally, movey
-else: COMP flagdir, 0
+ADD ballx, 1
+SUB bally, 1
+else: COMP flagdir, 1
+JUMP NZ, else2
+ADD ballx, 1
+ADD bally, 1
+else2: COMP flagdir, 2
+JUMP NZ, else3
+SUB ballx, 1
+ADD bally, 1
+else3: COMP flagdir, 3
 JUMP NZ, skip
-SUB ballx, movex
-SUB bally, movey
+SUB ballx, 1
+SUB bally, 1
 RET
 
 .CSEG 0x3FF
