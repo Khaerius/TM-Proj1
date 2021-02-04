f76e39e1d6342f16e914a8cb373d5ff3a50c20cd
Khaerius <33125280+Khaerius@users.noreply.github.com>
2021-02-04 14:22:11 +0100

    Update Joystick.asm

--- Joystick.asm
+++ Joystick.asm
@@ -1,23 +1,21 @@
 .PORT int_mask, 0xE1
 .PORT int_status, 0xE0
-.PORT joy, 0x21 		; Pmod B (gorny)
-.PORT joy_dir, 0x29 		
-.PORT joy_status, 0xE2
-.PORT joy_mask, 0xE4
+.PORT joyl, 0x21 		; Pmod B (gorny)
+.PORT joyl_dir, 0x29 		
+.PORT joyl_status, 0xE2
+.PORT joyl_mask, 0xE4
 .PORT leds, 0x00
 .REG sF, t1
 .REG sE, t2
 .REG sD, t3
 .REG sC, t4
-.REG s0, a
-.REG s1, b
 .REG s3, count
 .REG s4, tmp
 .REG sB, joymove
 
 
 LOAD s2, 0b00001011
-OUT s2, joy_dir
+OUT s2, joyl_dir
 
 loop:		
 		CALL cycle
@@ -29,47 +27,51 @@ do_step:
 		RET
 		
 sck_1:
-		IN b, joy
-		OR b, 8
-		OUT b, joy
-		CALL sleep_10ms
+		IN tmp, joyl
+		OR tmp, 8
+		OUT tmp, joyl
+		CALL sleep_100us
 		RET
 		
 sck_0:
-		IN b, joy
-		AND b, 0b11110111
-		OUT b, joy
-		CALL sleep_10ms
+		IN tmp, joyl
+		AND tmp, 0b11110111
+		OUT tmp, joyl
+		CALL sleep_100us
 		RET
 
 cycle:
-		LOAD b, 0
-		OUT b, joy
-		CALL sleep_10ms
+		LOAD tmp, 0
+		OUT tmp, joyl
+		CALL sleep_100us
 		LOAD count, 40
 	read:
  		CALL do_step
+		COMP count, 11
+		CALL Z, saveFirstPos
 		COMP count, 10
-		JUMP Z, saveFirstPos
-		COMP count, 9
-		JUMP Z, saveSecPos
+		CALL Z, saveSecPos
 		SUB count, 1
 		JUMP NZ, read
-		LOAD b, 1
-		OUT b, joy
+		LOAD tmp, 1
+		OUT tmp, joyl
 		OUT joymove, leds
 		RET
 
 saveFirstPos:
-	IN tmp, joy
-	TEST tmp, 4
-	LOAD joymove, NZ
+	IN tmp, joyl
+	LOAD joymove, 0
+	AND tmp, 0b00000100
+	SR0 tmp
+	ADD joymove, tmp
 	RET
 
 saveSecPos:
-	IN tmp, joy
-	TEST tmp, 4
-	LOAD joymove, NZ
+	IN tmp, joyl
+	AND tmp, 0b00000100
+	SR0 tmp
+	SR0 tmp
+	ADD joymove, tmp
 	RET
 ;EINT
 ;loop: CALL loop
@@ -88,20 +90,4 @@ wait_2:
  		CALL sleep_1us
 		SUB t2, 1
 		JUMP NZ, wait_2
-		RET
-		
-sleep_10ms:
- 		LOAD t3, 98
-wait_3:
- 		CALL sleep_100us
-		SUB t3, 1
-		JUMP NZ, wait_3
-		RET
-		
-sleep_1s:
- 		LOAD t4, 98
-wait_4:
- 		CALL sleep_10ms
-		SUB t4, 1
-		JUMP NZ, wait_4
 		RET
